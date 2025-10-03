811e6a70e75e175fdef17c74e6d6cf643bfb7134
Marco Frongia <marco.frongia.contatti@gmail.com>
2025-10-03 11:32:50 +0200

    server fixed

--- server/server.js
+++ server/server.js
@@ -1,41 +1,28 @@
-import express from "express";
-import cors from "cors";
-import { databaseUrl, localizedApi } from "../src/config.js";
+import { localizedApi } from "../src/config.js";
 
-const app = express();
-const PORT = 5000;
-
-app.use(cors());
-
-app.get("/api/word", async (req, res) => {
-  const lang = req.query.lang || "eng";
+export default async function handler(req, res) {
+  const { lang = "eng" } = req.query;
   const targetUrl = localizedApi[lang] || localizedApi["eng"];
 
   try {
     const response = await fetch(targetUrl);
     const data = await response.json();
-        if (response.ok) {
-          
-          let word;
-            switch (lang) {
-                case "eng":
-                    word= data[0]
-                    break;
-                case "ita":
-                    word= data[0].word
-                    break;
-                default:
-                    word = data[0];
-                }
 
-                    res.json(word);
-        }
+    let word;
+    switch (lang) {
+      case "eng":
+        word = data[0];
+        break;
+      case "ita":
+        word = data[0].word;
+        break;
+      default:
+        word = data[0];
+    }
+
+    res.status(200).json(word);
   } catch (error) {
     console.error("Errore nel proxy:", error);
     res.status(500).json({ error: "Errore nel proxy" });
   }
-});
-
-app.listen(PORT, () => {
-  console.log(`Proxy attivo su ${databaseUrl}`);
-});
+}
